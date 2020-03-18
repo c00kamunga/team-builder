d3bfd2e534e39802ea1ae93fd2294337056f69b4
c00kamunga <tsothalanti2@yahoo.com>
2020-03-17 19:30:35 -0700

    passed props down to MemberCard component, added JSX with dynamic value as props.member.name

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -52,3 +52,5 @@ const Form = props => {
     </form>
   );
 };
+
+export default Form;
--- team-builder/src/MemberCard.js
+++ team-builder/src/MemberCard.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function MemberCard() {
+export default function MemberCard(props) {
     return (
         <div>
-            
+         <h1>{props.member.name}</h1>
         </div>
     )
 }
