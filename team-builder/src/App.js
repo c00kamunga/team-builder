10cfbe977d0d1d2af2ca7ecc8cfaa2665f7c3c05
c00kamunga <tsothalanti2@yahoo.com>
2020-03-17 17:36:03 -0700

    replaced empty object for state in App component with array of objects with default data

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 
 function App() {
-  const [teamList, setTeamList] = useState({});
-  return <div className="App">
-    <h1>Member List</h1>
-  </div>;
+  const [teamList, setTeamList] = useState([
+    { name: "Barinder", role: "backend engineer", email: "haha@yahoo.com" },
+    { name: "Joseph", role: "frontend engineer", email: "wowow@yahoo.com" },
+    { name: "David", role: "UX Designer", email: "awesome@yahoo.com" }
+  ]);
+  return (
+    <div className="App">
+      <h1>Member List</h1>
+    </div>
+  );
 }
 
 export default App;
